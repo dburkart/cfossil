535b3618b6de6b245b52cc3beeab2bc64202d3f3
Dana Burkart <dana.burkart@gmail.com>
2022-12-21 19:47:55 -0800

    Add comments, and refactor the names of some functions.

--- fossil.h
+++ fossil.h
@@ -71,32 +71,47 @@ typedef struct fossil_response
     char *explanation;
 } fossil_response_t;
 
-typedef struct fossil_version_request
+//--
+//-- Concrete request objects
+//--
+
+typedef struct fossil_request_version
 {
     fossil_request_t base;
     char *version;
-} fossil_version_request_t;
+} fossil_request_version_t;
 
-typedef struct fossil_use_request
+typedef struct fossil_request_use
 {
     fossil_request_t base;
     char *db_name;
-} fossil_use_request_t;
+} fossil_request_use_t;
 
-typedef struct fossil_append_request
+typedef struct fossil_request_append
 {
     fossil_request_t base;
     uint32_t len;
     const char *topic;
     const char *data;
-} fossil_append_request_t;
+} fossil_request_append_t;
 
-typedef struct fossil_version_response
+//--
+//-- Concrete response objects
+//--
+
+typedef struct fossil_response_version
 {
     fossil_response_t base;
     char *version;
-} fossil_version_response_t;
+} fossil_response_version_t;
 
+/**
+ * Marshal a request, returning a packed message that can be used with
+ * fossil_message_write().
+ *
+ * @param req Address of a fossil request to marshal
+ * @return Packed message ready to be transmitted to the server
+ */
 fossil_message_t fossil_request_marshal(fossil_request_t *req)
 {
     fossil_message_t message;
@@ -105,14 +120,14 @@ fossil_message_t fossil_request_marshal(fossil_request_t *req)
 
     size_t len;
     uint32_t field_len = 0;
-    fossil_version_request_t *version_req;
-    fossil_use_request_t *use_req;
-    fossil_append_request_t *append_req;
+    fossil_request_version_t *version_req;
+    fossil_request_use_t *use_req;
+    fossil_request_append_t *append_req;
 
     switch (req->type)
     {
         case FOSSIL_REQ_VERSION:
-            version_req = (fossil_version_request_t *)req;
+            version_req = (fossil_request_version_t *)req;
             strcpy(message.command, "VERSION");
             len = strlen(version_req->version);
             message.data = malloc(len);
@@ -120,7 +135,7 @@ fossil_message_t fossil_request_marshal(fossil_request_t *req)
             message.len = len;
             break;
         case FOSSIL_REQ_USE:
-            use_req = (fossil_use_request_t *)req;
+            use_req = (fossil_request_use_t *)req;
             strcpy(message.command, "USE");
             len = strlen(use_req->db_name);
             message.data = malloc(len);
@@ -128,7 +143,7 @@ fossil_message_t fossil_request_marshal(fossil_request_t *req)
             message.len = len;
             break;
         case FOSSIL_REQ_APPEND:
-            append_req = (fossil_append_request_t *)req;
+            append_req = (fossil_request_append_t *)req;
             strcpy(message.command, "APPEND");
             len = strlen(append_req->topic) + strlen(append_req->data) + 4;
             field_len = strlen(append_req->topic);
@@ -144,11 +159,18 @@ fossil_message_t fossil_request_marshal(fossil_request_t *req)
     return message;
 }
 
+/**
+ * Unmarshal a response from the server. This function will dynamically allocate
+ * any memory needed to hold the response object
+ *
+ * @param message Address of the message to read into
+ * @return Fossil response object
+ */
 fossil_response_t *fossil_response_unmarshal(fossil_message_t *message)
 {
     if (strcmp(message->command, "VERSION") == 0)
     {
-        fossil_version_response_t *version_response = malloc(sizeof(fossil_version_response_t));
+        fossil_response_version_t *version_response = malloc(sizeof(fossil_response_version_t));
         version_response->base.type = FOSSIL_RESP_VERSION;
         version_response->base.code = *((uint32_t *)message->data);
         version_response->version = malloc(message->len - 4);
@@ -180,6 +202,12 @@ fossil_response_t *fossil_response_unmarshal(fossil_message_t *message)
 }
 
 
+/**
+ * Free the data associated with a given fossil message. This should always be called
+ * after calling fossil_message_read / fossil_message_write.
+ *
+ * @param message Address of the message to free
+ */
 void fossil_message_free(fossil_message_t *message)
 {
     if (message->data != NULL) {
@@ -189,7 +217,15 @@ void fossil_message_free(fossil_message_t *message)
     }
 }
 
-ssize_t fossil_read_message(fossil_client_t *client, fossil_message_t *message)
+/**
+ * Read a message from the database server. This reads a raw message off the wire,
+ * and does not process it at all.
+ *
+ * @param client Client connection
+ * @param message Pointer to a message to fill in
+ * @return Number of bytes read
+ */
+ssize_t fossil_message_read(fossil_client_t *client, fossil_message_t *message)
 {
     ssize_t result;
     // First, read the length of the message, which is the first 4 bytes
@@ -211,7 +247,16 @@ ssize_t fossil_read_message(fossil_client_t *client, fossil_message_t *message)
     return result;
 }
 
-ssize_t fossil_write_message(fossil_client_t *client, fossil_message_t *message)
+/**
+ * Write a packed message to the database. This function should only be used with
+ * a packed message containing a marshaled request. In most cases, a client should
+ * be using fossil_send().
+ *
+ * @param client Client connection
+ * @param message Message to send to the database
+ * @return Number of bytes written to the client
+ */
+ssize_t fossil_message_write(fossil_client_t *client, fossil_message_t *message)
 {
     ssize_t result;
     // First, write the length of the message
@@ -230,17 +275,24 @@ ssize_t fossil_write_message(fossil_client_t *client, fossil_message_t *message)
     return len;
 }
 
+/**
+ * Send a request to the database.
+ *
+ * @param client Client connection
+ * @param request Request to send to the database
+ * @return fossil_response_t representing the server's response
+ */
 fossil_response_t *fossil_send(fossil_client_t *client, fossil_request_t *request)
 {
     fossil_response_t *response = NULL;
     fossil_message_t message, server_response = { .data = NULL};
 
     message = fossil_request_marshal(request);
-    if (fossil_write_message(client, &message) < 0)
+    if (fossil_message_write(client, &message) < 0)
         goto cleanup;
 
 
-    fossil_read_message(client, &server_response);
+    fossil_message_read(client, &server_response);
     response = fossil_response_unmarshal(&server_response);
 
 cleanup:
@@ -250,12 +302,27 @@ cleanup:
     return response;
 }
 
+/**
+ * Append data to a fossil database.
+ *
+ * @param client Client connection
+ * @param topic A topic string specifying which topic the data should be appended to
+ * @param data Bytes to append
+ * @param len Number of bytes to be appended
+ * @return fossil_response_t representing the server's response
+ */
 fossil_response_t *fossil_append(fossil_client_t *client, const char *topic, const char *data, uint32_t len)
 {
-    fossil_append_request_t request = { .base.type = FOSSIL_REQ_APPEND, .topic = topic, .data = data, .len = len};
+    fossil_request_append_t request = { .base.type = FOSSIL_REQ_APPEND, .topic = topic, .data = data, .len = len};
     return fossil_send(client, (fossil_request_t *)&request);
 }
 
+/**
+ * Connect to a fossil database.
+ *
+ * @param client Address of an empty client to fill in
+ * @return -1 if an error occurred
+ */
 ssize_t fossil_connect(fossil_client_t *client)
 {
     ssize_t result = 0;
@@ -281,10 +348,10 @@ ssize_t fossil_connect(fossil_client_t *client)
     }
 
     // Now that we're connected, send a version advertisement
-    fossil_version_request_t advertisement = {.base.type = FOSSIL_REQ_VERSION};
+    fossil_request_version_t advertisement = {.base.type = FOSSIL_REQ_VERSION};
     advertisement.version = PROTOCOL_VERSION;
 
-    fossil_version_response_t *server_version = (fossil_version_response_t *)fossil_send(client,(fossil_request_t *) &advertisement);
+    fossil_response_version_t *server_version = (fossil_response_version_t *)fossil_send(client, (fossil_request_t *) &advertisement);
 
     if (server_version == NULL)
         goto cleanup;
@@ -297,7 +364,7 @@ ssize_t fossil_connect(fossil_client_t *client)
 
     // Now use the default database
     // FIXME: This should be configurable.
-    fossil_use_request_t use_req = {.base.type = FOSSIL_REQ_USE, .db_name = "default"};
+    fossil_request_use_t use_req = {.base.type = FOSSIL_REQ_USE, .db_name = "default"};
     fossil_response_t *use_resp = fossil_send(client, (fossil_request_t *)&use_req);
 
     if (use_resp->type != FOSSIL_RESP_OK)
